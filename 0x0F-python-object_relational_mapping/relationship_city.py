c2744c88bae3ed24496ecd8da1628231d2279d7d
amykad <amina.kaddal.1@gmail.com>
2024-04-12 22:25:03 +0000

    update

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Lists states"""
+"""Defines a class City"""
+
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from relationship_state import Base
+from relationship_state import Base, State
+
 
 class City(Base):
-    """Class representing the states table"""
-    __tablename__ = 'cities'
-
-    id = Column(Integer, nullable=False, primary_key=True,
-                autoincrement=True, unique=True)
+    """Module of a city"""
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""Lists states"""
+"""Defines a class State"""
+
 
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref
 
 Base = declarative_base()
 
 
 class State(Base):
-    """Class representing the states table"""
+    """Module of a state"""
     __tablename__ = 'states'
-
-    id = Column(Integer, nullable=False, primary_key=True,
-                autoincrement=True, unique=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-
-    cities = relationship(
-        "City",
-        cascade="all, delete-orphan",
-        backref=backref("state", cascade="all"),
-        single_parent=True)
+    cities = relationship("City", backref="state")
