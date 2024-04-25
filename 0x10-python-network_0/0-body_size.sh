1e23ab0901cc6f3054787c10a1277b5efc4a514e
amykad <amina.kaddal.1@gmail.com>
2024-04-25 21:35:46 +0100

    update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
-
-# Check if the user has provided a URL
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Assign the URL provided as a command-line argument to a variable
-URL=$1
-
-# Use curl to send a request to the URL and store the response body in a temporary file
-RESPONSE=$(curl -s -o temp_response.txt -w "%{size_download}" $URL)
-
-# Display the size of the response body in bytes
-echo "Size of the response body: $RESPONSE bytes"
-
-# Clean up the temporary file
-rm temp_response.txt
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI $1 | grep "Content-Length" | cut -d " " -f2
